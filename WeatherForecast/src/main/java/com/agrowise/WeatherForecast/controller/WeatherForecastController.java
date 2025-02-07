d150372517e995023e863526a71b23731c957359
jaspass04 <t8220122@aueb.gr>
2025-02-07 04:49:08 +0200

    Replace javax.servlet with jakarta.servlet because first one doesnt exist anymore

--- WeatherForecast/src/main/java/com/agrowise/WeatherForecast/controller/WeatherForecastController.java
+++ WeatherForecast/src/main/java/com/agrowise/WeatherForecast/controller/WeatherForecastController.java
@@ -1,4 +1,20 @@
 package com.agrowise.WeatherForecast.controller;
 
+import com.agrowise.WeatherForecast.service.WeatherForecastService;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import jakarta.servlet.http.HttpSession;
+
+@Controller
+@RequestMapping("/weather")
 public class WeatherForecastController {
+    private WeatherForecastService weatherForecastService;
+     @GetMapping
+    public String weatherForecast(Model model, HttpServletRequest request) {
+         Object user = session.getAttribute("userObj2024");
+
+     }
 }
